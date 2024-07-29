e8e3d9e9069fc754ae06ea87e931ced1a92646d1
unknown <jakkulachandu078@gmail.com>
2024-07-29 14:48:38 +0530

    first commit

---
+++ Main.java
@@ -0,0 +1,4 @@
+public class Main{
+public ststic void main(String[] args){[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[a[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C
+system.out.println("Hello GIT"[C[D);
+}
